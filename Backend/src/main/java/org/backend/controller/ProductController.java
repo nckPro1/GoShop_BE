5ff4b52c320158fb7aff2436a81dc4a48310fd2c
NCK <kien2862004@gmail.com>
2025-12-13 02:49:52 +0700

    create product

--- Backend/src/main/java/org/backend/controller/ProductController.java
+++ Backend/src/main/java/org/backend/controller/ProductController.java
@@ -1,4 +1,42 @@
 package org.backend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.backend.model.Product;
+import org.backend.service.ProductService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductService productService;
+
+    // ai cũng được đọc
+    @GetMapping
+    @PreAuthorize("hasAuthority('PRODUCT_READ')")
+    public List<Product> getAll() {
+        return productService.getAll();
+    }
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('PRODUCT_CREATE')")
+    public Product create(@RequestBody Product p) {
+        return productService.create(p);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAuthority('PRODUCT_UPDATE')")
+    public Product update(@PathVariable Long id, @RequestBody Product p) {
+        return productService.update(id, p);
+    }
+
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasAuthority('PRODUCT_DELETE')")
+    public void delete(@PathVariable Long id) {
+        productService.delete(id);
+    }
 }
--- Backend/src/main/java/org/backend/repository/ProductRepository.java
+++ Backend/src/main/java/org/backend/repository/ProductRepository.java
@@ -1,4 +1,6 @@
 package org.backend.repository;
 
-public class ProductRepository {
-}
+import org.backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {}
